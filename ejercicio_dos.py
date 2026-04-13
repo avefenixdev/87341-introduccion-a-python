1072b5fdd00903247ad68c9608ad263974dd5642
Ave Fenix Dev <avefenixdev@gmail.com>
2026-04-13 16:04:59 -0300

    Refactor del enunciado

--- ejercicio_dos.py
+++ ejercicio_dos.py
@@ -5,6 +5,7 @@ El programa debe:
 
 1. Pedir una cantidad de pesos argentinos
 2. Convertir a dólares (tipo de cambio: U$S1 = $1400)
-3. Mostrar cuántos dólares puedo comprar.
+3. Mostrar cuántos dólares puedo comprar. (Concatenado) 
+3.1. "15000 pesos es igual 100 dólares"
 
  """
