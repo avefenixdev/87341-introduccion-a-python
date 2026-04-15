00ca5fb40c3b8bc09849a84cda12633c77b2e9c4
Ave Fenix Dev <avefenixdev@gmail.com>
2026-04-15 16:03:02 -0300

    Enunciado ejercicio

---
+++ clase-02/main.py
@@ -0,0 +1,9 @@
+# Ejercicio: Listas
+## Consigna: Crear una lista de números. Y luego:
+
+# - [10, 22, 55, 2, 88, 7]
+
+# - Imprimir cada número | imprimir(lista)
+# - Calcular la suma de todos | suma = calcular_suma(lista)
+# - Calcular el promedio | promedio = calcular_promedio(suma, lista)
+# - Mostrar la suma de los números y el promedio
