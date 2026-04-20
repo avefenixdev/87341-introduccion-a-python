d3ef7edfde2ee1ea20dc99d33df2f422b417dff0
Ave Fenix Dev <avefenixdev@gmail.com>
2026-04-20 14:17:18 -0300

    Empezando con la clase 04

---
+++ clase-04/ejercicio/main.py
@@ -0,0 +1,3 @@
+# Crear un proyecto con archivos
+#   * operaciones.py (funciones para sumar, restar y multiplicar)
+#   * main.py (usar esas funciones)
---
+++ clase-04/ejercicio/operaciones.py
@@ -0,0 +1,9 @@
+def sumar(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
