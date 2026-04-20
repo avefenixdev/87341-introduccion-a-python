12255d00186510c99e10f08926221858b87a36b4
Ave Fenix Dev <avefenixdev@gmail.com>
2026-04-20 14:35:35 -0300

    Mejora del enunciado

--- clase-04/ejercicio-alumnos/main.py
+++ clase-04/ejercicio-alumnos/main.py
@@ -7,3 +7,7 @@
 
 # calcular.py - Vamos a tener 2 archivos (el de la función)
 # main.py - Orquesta los modulos
+
+# El resultado esperado luego de la ejecución del programa
+
+# { 'suma': 75, 'promedio': 15.0, 'mayor': 25, 'menor: '5'}
